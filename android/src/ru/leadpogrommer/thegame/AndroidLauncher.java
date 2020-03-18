6ec76fa8f32a1f88238cb598c340a1cf5b9f06d4
leadpogrommer <ilyamerzlakov@gmail.com>
2020-03-18 17:55:33 +0700

    initial commit

---
+++ android/src/ru/leadpogrommer/thegame/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package ru.leadpogrommer.thegame;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import ru.leadpogrommer.thegame.TheGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new TheGame(), config);
+	}
+}
---
+++ core/src/ru/leadpogrommer/thegame/TheGame.kt
@@ -0,0 +1,29 @@
+package ru.leadpogrommer.thegame
+
+import com.badlogic.gdx.ApplicationAdapter
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.GL20
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+
+class TheGame : ApplicationAdapter() {
+    var batch: SpriteBatch? = null
+    var img: Texture? = null
+    override fun create() {
+        batch = SpriteBatch()
+        img = Texture("badlogic.jpg")
+    }
+
+    override fun render() {
+        Gdx.gl.glClearColor(0.5f, 1f, 0f, 1f)
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+        batch!!.begin()
+        batch!!.draw(img, 0f, 0f)
+        batch!!.end()
+    }
+
+    override fun dispose() {
+        batch!!.dispose()
+        img!!.dispose()
+    }
+}
